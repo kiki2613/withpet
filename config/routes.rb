bd11a8045492a07f10a448eddb54b1c5f48dc117
kiki2613 <keywnm@gmail.com>
2024-06-26 14:45:50 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root_path "cafes#index"
+  resources :cafes, only: :index
 end
