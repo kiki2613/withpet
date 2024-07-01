148e60e49f90757c768a818d67d84c50434b797c
kiki2613 <keywnm@gmail.com>
2024-07-01 19:36:50 +0900

    shop.rbの編集

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,12 +1,14 @@
 class Shop < ApplicationRecord
+  has_many :favorites
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
 
   def self.ransackable_attributes(auth_object = nil)
     ["address", "nearest_station"]
   end
 
-  has_many :favorites
-  
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :prefecture
-
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 end
