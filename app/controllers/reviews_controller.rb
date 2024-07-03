8c115b8f20f2724c04e030f9b49c83ca3568e559
kiki2613 <keywnm@gmail.com>
2024-07-03 17:53:39 +0900

    レビュー投稿画面作成

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,26 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_shop
+
+  def new
+    @review = @shop.reviews.new
+  end
 
   def create
-    @reviews = Review.create(review_params)
-    redirect_to shop_path(@reviews.shop.id)
+    @reviews = Review.new(review_params)
+
+    if @reviews.save
+      redirect_to shop_path(@reviews.shop.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
+
+  def find_shop
+    @shop = Shop.find(params[:shop_id])
+  end
 
   def review_params
     params.require(:review).permit(:visit_date, 
