fde7a66d898c4df0cc78419f1621252aaf4422f6
kiki2613 <keywnm@gmail.com>
2024-07-03 17:05:43 +0900

    userコントローラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_user
+  before_action :current_user?
+ 
   def show
+  end
+
+  private
+
+  def find_user
     @user = User.find(params[:id])
+  end
+
+  def current_user?
+    unless current_user == @user
+      redirect_to root_path
+    end
   end
 end
