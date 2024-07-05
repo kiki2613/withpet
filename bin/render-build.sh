2fd17d04c65284f64cf8d2a630a44f103e5dda6c
kiki2613 <keywnm@gmail.com>
2024-07-05 14:03:31 +0900

    seeds.rbの反映

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,4 +8,4 @@ bundle exec rake assets:clean
 # bundle exec rake db:migrate
 
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
-# bundle exec rake db:seed 
+bundle exec rake db:seed 
