6f6429baf30a24f8fe697132fb9606340f6a3358
kiki2613 <keywnm@gmail.com>
2024-07-05 10:13:33 +0900

    レビュー削除機能

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :index
   before_action :find_shop, except: :index
 
   def new
@@ -15,10 +15,10 @@ class ReviewsController < ApplicationController
       if review_count < 1
         @review.save
         flash[:notice] = "レビューを投稿しました"
-        redirect_to shop_path(@review.shop.id)
+        redirect_to shop_path(@shop)
       else
         flash[:notice] = "レビューの投稿は一度までです！"
-        redirect_to shop_path(@review.shop.id)
+        redirect_to shop_path(@shop)
       end
     else
       render :new, status: :unprocessable_entity
@@ -30,10 +30,10 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
-    @review = Review.find(@shop)
+    @review = @shop.reviews.find(params[:id])
     if @review.destroy
       flash[:notice] = "レビューを削除しました"
-      redirect_to root_path
+      redirect_to shop_path(@shop)
     else
       flash[:notice] = "問題が起きて削除できませんでした"
       redirect_to shop_path(@shop)
