a8c58d55e75cc5bca504c9649befd94876c0e1d6
kiki2613 <keywnm@gmail.com>
2024-07-27 11:27:33 +0900

    factories//users.rbの編集

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.email }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    age_id                { Faker::Number.between(from: 1, to: 3) }
+    gender_id             { Faker::Number.between(from: 1, to: 3) }
   end
 end
