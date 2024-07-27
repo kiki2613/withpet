b30a2016f0bed8d160ec29e764fc4a52084879da
kiki2613 <keywnm@gmail.com>
2024-07-27 15:16:01 +0900

    README編集
    マイグレーションファイル編集によるカラム変更のため

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Shop, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @shop = FactoryBot.build(:shop)
+  end
+
+  describe '店舗情報の登録' do
+    context '店舗情報の登録ができるとき' do
+      it '' do
+      end
+      it '' do
+      end
+      it '' do
+      end
+    end
+    context '店舗情報の登録ができないとき' do
+      it '' do
+      end
+      it '' do
+      end
+      it '' do
+      end
+    end
+  end
 end
