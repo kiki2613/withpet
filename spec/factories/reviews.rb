990b3bca850768a582f4ae9ed26323c311131658
kiki2613 <keywnm@gmail.com>
2024-07-28 17:32:45 +0900

    factories/reviews.rbの編集

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :review do
-    
+    visit_date  { Faker::Date.backward(days: 365) }
+    comment     { Faker::Lorem.paragraph }
+    association :user
+    association :shop
+
+    after(:build) do |review|
+      review.image.attach(io: File.open('public/images/shop_image_test.png'), filename: 'shop_image_test.png')
+    end
   end
 end
