47400bb7d48f640a1405e81e62880ff76dc5aff0
kiki2613 <keywnm@gmail.com>
2024-07-30 17:53:18 +0900

    favoriteモデルのテスト実施

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+    @shop = FactoryBot.build(:shop)
+    @favorite = FactoryBot.build(:favorite, user: @user, shop: @shop)
+  end
+
+  describe '' do
+    context 'お気に入り登録できるとき' do
+      it 'user_id、shop_idがあるとき' do
+        expect(@favorite).to be_valid
+      end
+    end
+
+    context 'お気に入り登録できないとき' do
+      it 'userと紐づいていないとき' do
+        @favorite.user = nil
+        @favorite.valid?
+        expect(@favorite.errors.full_messages).to include('User must exist')
+      end
+
+      it 'shopと紐づいていないとき' do
+        @favorite.shop = nil
+        @favorite.valid?
+        expect(@favorite.errors.full_messages).to include('Shop must exist')
+      end
+
+      it 'すでにその店舗がお気に入り登録されているとき' do
+        @favorite.save
+        duplicate_favorite = FactoryBot.build(:favorite, user: @user, shop: @shop)
+        duplicate_favorite.valid?
+        expect(duplicate_favorite.errors.full_messages).to include('User has already been taken')
+      end
+    end
+  end
 end
