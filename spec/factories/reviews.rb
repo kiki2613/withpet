6ae1841ed385a7d47e36b7c61ba81d581bbcd7c6
kiki2613 <keywnm@gmail.com>
2024-07-30 17:06:03 +0900

    reviewモデルのテスト実施

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     association :shop
 
     after(:build) do |review|
-      review.image.attach(io: File.open('public/images/shop_image_test.png'), filename: 'shop_image_test.png')
+      review.image.attach(io: File.open('public/images/shop_image_test.jpg'), filename: 'shop_image_test.jpg')
     end
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @review = FactoryBot.build(:review)
+  end
+
+  describe '' do
+    context 'レビューが投稿できるとき' do
+      it 'visit_date、commentがあるとき' do
+        expect(@review).to be_valid
+      end
+
+      it 'imageがなくても保存できる' do
+        @review.image = nil
+        expect(@review).to be_valid
+      end
+    end
+
+    context 'レビュー投稿できないとき' do
+      it 'visit_dateが空のとき' do
+        @review.visit_date = ''
+        @review.valid?
+        expect(@review.errors.full_messages).to include("Visit date can't be blank")
+      end
+
+      it 'commentが空のとき' do
+        @review.comment = ''
+        @review.valid?
+        expect(@review.errors.full_messages).to include("Comment can't be blank")
+      end
+
+      it 'userと紐づいていないとき' do
+        @review.user = nil
+        @review.valid?
+        expect(@review.errors.full_messages).to include('User must exist')
+      end
+
+      it 'shopと紐づいていないとき' do
+        @review.shop = nil
+        @review.valid?
+        expect(@review.errors.full_messages).to include('Shop must exist')
+      end
+    end
+  end
 end
